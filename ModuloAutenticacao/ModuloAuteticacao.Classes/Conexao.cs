2ae205f69ef17aae1edd3bfc8ff8b3abbcf06a5f
Ramon Vieira Miranda <ramon.miranda@aluno.senai.br>
2022-08-02 09:36:38 -0300

    segundo commit

--- ModuloAutenticacao/ModuloAuteticacao.Classes/Conexao.cs
+++ ModuloAutenticacao/ModuloAuteticacao.Classes/Conexao.cs
@@ -6,10 +6,20 @@ namespace ModuloAuteticacao.Classes
     internal class Conexao
     {
 
-        public static DbConnection _conn;
+        public static SqlConnection _conn;
 
-        public static SqlConnection  MinhaInstacia { get; set; }
-
-
+        public SqlConnection MinhaInstancia
+        { //a chave da propriedade
+            get
+            { // chave do método get
+              //se não existe conexão.
+                if (_conn == null)
+                { // chave do if
+                    _conn = new SqlConnection(@"Server = localhost; Database = projetoestoquev; Uid = sa; Pwd = teste*123;");
+                } // fecha chave do if
+                  //retorna a conexão
+                return _conn;
+            }
+        }
     }
 }
